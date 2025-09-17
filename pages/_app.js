6e03c71cebc9c1a0eb328c1cc442984c47cb7f77
Ray Christian Santos <qqqraysantos@gmail.com>
2025-09-17 10:31:18 +0800

    withloginfixed2

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,12 @@
+// pages/_app.js
 import '../styles/globals.css'
 import { AuthProvider } from '../components/AuthProvider'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <script src="https://cdn.tailwindcss.com"></script>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </>
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
   )
 }
 
